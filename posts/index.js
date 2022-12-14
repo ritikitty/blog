2691c7cbc6782733dccc8f356aee41d82ef98f31
Orell Hürzeler <orell.huerzeler@eventfrog.net>
2022-12-14 20:35:48 +0100

    implementation of comments and posts service

--- posts/index.js
+++ posts/index.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { randomBytes } = require('crypto')
+const cors = require('cors')
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 
 const posts = {}
 
-app.get('/posts'),
-  (req, res) => {
-    res.send(posts)
-  }
+app.get('/posts', (req, res) => {
+  res.send(posts)
+})
 
 app.post('/posts', (req, res) => {
   const id = randomBytes(4).toString('hex')
